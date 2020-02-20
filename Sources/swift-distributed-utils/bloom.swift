aae1a5ccc0721b04068d00e37d9ca0e81cb514ab
Chris Mowforth <chris@mowforth.com>
2020-02-20 20:50:52 +0000

    initial stab at bloom filter impl

---
+++ Sources/swift-distributed-utils/bloom.swift
@@ -0,0 +1,103 @@
+//
+//  bloom.swift
+//
+//
+//  Created by Chris Mowforth on 20/02/2020.
+//
+
+import Foundation
+import xxHash_Swift
+
+public protocol BloomFilter : AnyObject {
+    func put(item: String)
+    func mightContain(item: String) -> Bool
+}
+
+// Based on the principle outlined in 'Less Hashing, Same Performance' by Kirsch & Mitzenmacher
+// Rather than 16 separate functions, we take the output of one 64 bit fn and accumulate
+// each 32-bit half with the loop variable to get the index to flip
+//
+// See https://www.eecs.harvard.edu/~michaelm/postscripts/rsa2008.pdf
+public final class NaiveBloomFilter {
+
+    private var array: [Bool]
+    private let hashingRounds: Int
+
+    public init() {
+        self.array = [Bool](repeating: false, count: 4096)
+        self.hashingRounds = 16
+    }
+
+    public func put(item: String) {
+        let (lowerHalf, upperHalf) = getInitialHash(item)
+        for i in 1...hashingRounds {
+            let combined = lowerHalf + (upperHalf * UInt64(i))
+            self.array[Int(combined) % array.count] = true
+        }
+    }
+
+    public func mightContain(item: String) -> Bool {
+        let (lowerHalf, upperHalf) = getInitialHash(item)
+        var count = 0
+        for i in 1...hashingRounds {
+            let combined = lowerHalf + (upperHalf * UInt64(i))
+            if self.array[Int(combined) % array.count] {
+                count = count + 1
+            }
+        }
+        return count == hashingRounds
+    }
+
+    private func getInitialHash(_ item: String) -> (UInt64, UInt64) {
+        let hash = XXH64.digest(item).littleEndian
+        let lowerHalf = hash & 0xFFFFFFFF
+        let upperHalf = (hash >> 32) & 0xFFFFFFFF
+        return (lowerHalf, upperHalf)
+    }
+}
+
+// Same mechanism above to reduce complexity of hashing rounds,
+// but all arithmetic is done using SIMD vectors. Aside from the final loop,
+// this lets you alter hashing rounds without a linear increase in number of arithmetic instructions,
+// on my MPB this performs approximately ~2x faster as the Naive Impl for typical collections
+public final class SIMDBloomFilter : BloomFilter {
+
+    private static let initialMask: SIMD16<UInt64> = SIMD16<UInt64>(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
+    private static let zeroMask: SIMD16<UInt64> = SIMD16<UInt64>()
+
+    private var array: [Bool]
+    private let hashingRounds: Int
+
+    public init() {
+        self.array = [Bool](repeating: false, count: 4096)
+        self.hashingRounds = 16
+    }
+
+    public func put(item: String) {
+        let indices = getHashMask(item)
+        for i in 0..<hashingRounds {
+            self.array[Int(indices[i])] = true
+        }
+    }
+
+    public func mightContain(item: String) -> Bool {
+        let indices = getHashMask(item)
+        var count = 0
+        for i in 0..<hashingRounds {
+            if self.array[Int(indices[i])] {
+                count = count + 1
+            }
+        }
+        return count == hashingRounds
+    }
+
+    private func getHashMask(_ item: String) -> SIMD16<UInt64> {
+        let hash = XXH64.digest(item).littleEndian
+        let lowerHalf = hash & 0xFFFFFFFF
+        let upperHalf = (hash >> 32) & 0xFFFFFFFF
+
+        let multiplied = SIMDBloomFilter.initialMask &* upperHalf
+        let added = multiplied &+ lowerHalf
+        return added % UInt64(array.count)
+    }
+}
--- Sources/swift-distributed-utils/main.swift
+++ Sources/swift-distributed-utils/main.swift
@@ -1,7 +1,35 @@
-print("Hello, world!")
+import Foundation
 
 let ch = ConsistentHashRing()
 
 ch.addNode("node0")
 
 let t = BinaryTree<Int>()
+
+func printTime() {
+    let currentDateTime = Date()
+    print("\(currentDateTime)")
+}
+
+let bf = NaiveBloomFilter()
+
+printTime()
+
+for _ in 0...100000000 {
+    bf.put(item: "hi")
+}
+
+printTime()
+
+//print("\(bf.mightContain(item: "hi"))")
+//print("\(bf.mightContain(item: "wibble"))")
+
+let sbf = SIMDBloomFilter()
+
+for _ in 0...100000000 {
+    sbf.put(item: "hi")
+}
+printTime()
+
+print("\(sbf.mightContain(item: "hi"))")
+print("\(sbf.mightContain(item: "wibble"))")
