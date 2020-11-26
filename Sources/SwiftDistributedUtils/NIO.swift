2c02ef111a77188c7fcf701bb5b5d2d4f567b38c
Chris Mowforth <chris@mowforth.com>
2020-11-26 17:33:43 -0300

    add convenience extension for TimeAmount -> TimeInterval

--- Sources/SwiftDistributedUtils/NIO.swift
+++ Sources/SwiftDistributedUtils/NIO.swift
@@ -34,13 +34,22 @@ public final class FixedRetryStrategy: RetryStrategy {
 
 }
 
+extension TimeAmount {
+
+    public var timeInterval: TimeInterval {
+        get {
+            return Double(nanoseconds / 1000000000)
+        }
+    }
+}
+
 public extension ClientBootstrap {
 
     func connectWithRetry(host: String, port: Int, strategy: RetryStrategy) -> EventLoopFuture<Channel> {
         return connect(host: host, port: port).flatMapError { err in
             print("oh noes! trying again")
             if let amount = strategy.timeToWait {
-                Thread.sleep(forTimeInterval: Double(amount.nanoseconds / 1000000000))
+                Thread.sleep(forTimeInterval: amount.timeInterval)
                 return self.connectWithRetry(host: host, port: port, strategy: strategy.nextTransition)
             } else {
                 return self.connect(host: host, port: port)
