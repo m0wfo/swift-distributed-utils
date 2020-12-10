e43d583cc26f591475f140ea12c20176a7d54648
Chris Mowforth <chris@mowforth.com>
2020-12-09 22:40:51 -0300

    log error reason on connection failure

--- Sources/SwiftDistributedUtils/NIO.swift
+++ Sources/SwiftDistributedUtils/NIO.swift
@@ -50,7 +50,7 @@ public extension ClientBootstrap {
 
     func connectWithRetry(host: String, port: Int, strategy: RetryStrategy) -> EventLoopFuture<Channel> {
         return connect(host: host, port: port).flatMapError { err in
-            Logger(label: "ClientBootstrapExtension").warning("Unable to connect to \(host):\(port)")
+            Logger(label: "ClientBootstrapExtension").warning("Unable to connect to \(host):\(port), error: \(err.localizedDescription)")
             if let amount = strategy.timeToWait {
                 Thread.sleep(forTimeInterval: amount.timeInterval)
                 return self.connectWithRetry(host: host, port: port, strategy: strategy.nextTransition)
